dd4bce60770e3c1ab7f0cea16c6057f486b0d808
Bangyou Zheng <bangyou.zheng@csiro.au>
2024-06-27 08:57:49 +1000

    Add multiple files

---
+++ R/options.R
@@ -0,0 +1,40 @@
+# * Author:    Bangyou Zheng (Bangyou.Zheng@csiro.au)
+# * Created:   03:40 PM Saturday, 09 June 2018
+# * Copyright: AS IS
+
+
+
+# Variable, global to package's namespace.
+# This function is not exported to user space and does not need to be documented.
+TWS_OPTIONS <- settings::options_manager(
+    host = "http://127.0.0.1:8080/"
+)
+
+
+#' Set or get options for my package
+#'
+#' @param ... Option names to retrieve option values or \code{[key]=[value]} pairs to set options.
+#'
+#' @section Supported options:
+#' The following options are supported
+#'  host: host of tiddlywiki
+#'
+#' @return the default and modified options.
+#' @export
+#' @examples
+#' tws_options(host = "http://127.0.0.1:8080/")
+tws_options <- function(...){
+    # protect against the use of reserved words.
+    settings::stop_if_reserved(...)
+    TWS_OPTIONS(...)
+}
+
+#' Reset global options for pkg
+#'
+#' @return the default options
+#' @export
+#' @examples
+#' tws_reset()
+tws_reset <- function() {
+    settings::reset(TWS_OPTIONS)
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
+
+> [!CAUTION]
+> This package will modify and delete tidders in Tiddlywiki. Use it with caution. 
+> Create a backup and test it before using,
+
 # tw-rscripts
-R Scripts for Tiddlywiki
+
+R package `twrscripts` is a collections of R Scripts for Tiddlywiki to manage
+
+* Colleague
+* reference
+
+
+## Data structure
+
+`Colleague` refers to a person in a real life with tag `Colleague` and following fields which are used 
+in this package.
+
+* `url` for personal webpage, with assumption to contain a list of publications.
+* `google-scholar` for personal profile to [Google Schole](https://scholar.google.com).
