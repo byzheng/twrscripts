6f9e01f5452666cc1b9750d88f3f251fd5488f10
Bangyou Zheng <bangyou.zheng@csiro.au>
2024-07-01 23:08:53 +1000

    Convert title into chinese

---
+++ R/chinese.R
@@ -0,0 +1,34 @@
+
+#' Convert tiddlers with Chinese title into pinyin
+#'
+#' @details
+#' The converted pinyin is merged into field `aka`. An extra field `py-checked` is added to indicate
+#' this tiddler is checked and will skip in the future.
+#'
+#' @return No return values
+#' @export
+convert_pinyin <- function() {
+    mypy <- pinyin::pydic(method = 'toneless', dic = c("pinyin2"))
+    all_tiddlers <- rtiddlywiki::get_tiddlers("[all[tiddlers]!search:title:regexp[\\w+]] +[!has:field[py-checked]]")
+
+    i <- 1
+    for (i in seq(along = all_tiddlers)) {
+        if (!grepl("[a-zA-Z _]", all_tiddlers[[i]]$title)) {
+            if (!is.null(all_tiddlers[[i]]$`py-checked`)) {
+                message("Skip for ", all_tiddlers[[i]]$title)
+                next
+            }
+            message(all_tiddlers[[i]]$title)
+            pinyin_i <- pinyin::py(all_tiddlers[[i]]$title, dic = mypy, sep = "")
+            message(pinyin_i)
+            akas <- unique(c(pinyin_i,
+                             rtiddlywiki::split_field(all_tiddlers[[i]]$aka)))
+            #akas <- pinyin_i
+            rtiddlywiki::put_tiddler(all_tiddlers[[i]]$title,
+                                     fields = list(aka = akas,
+                                                   `py-checked`=""))
+
+        }
+    }
+    return(invisible())
+}
